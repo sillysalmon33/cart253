363c07f65feab40d03af6ab5db7cf70290cb518b
sillysalmon33 <sillysalmon33@icloud.com>
2026-09-10 20:33:30 -0400

    did function call section

--- tests/2 function calls/template-p5-project/template-p5-project/js/script.js
+++ tests/2 function calls/template-p5-project/template-p5-project/js/script.js
@@ -12,13 +12,16 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+ createCanvas(640, 480);
 }
+
 
 
 /**
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-
+background(255, 100, 100);
+rect(200, 80, 240, 320);
 }
+
