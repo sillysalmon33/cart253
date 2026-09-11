03460631cf3984af43f45fcbd737af61f3e4a1d5
sillysalmon33 <sillysalmon33@icloud.com>
2026-09-11 16:12:39 -0400

    finished house example

--- tests/3 house/starter-house/starter-house/js/script.js
+++ tests/3 house/starter-house/starter-house/js/script.js
@@ -22,15 +22,27 @@ function setup() {
  * Draws a house
  */
 function draw() {
+    drawSky();
+    drawCloud();
+    drawGround();
+    drawHouse();
+}
+
+/**
+ * Draws the sky (nice and blue)
+ */
+function drawSky() {
     // The sky
     background(150, 200, 250);
+}
 
+/**
+ * Draws a fluffy white cloud
+ */
+function drawCloud() {
     // A cloud
     push();
     noStroke();
-    // Note: using a single number for a colour will be greyscale
-    // (As if you used the same number for R, G, and B)
-    // So this is white:
     fill(255);
     ellipse(100, 100, 100, 100);
     ellipse(180, 80, 100, 100);
@@ -38,41 +50,66 @@ function draw() {
     ellipse(190, 130, 60, 60);
     ellipse(220, 120, 60, 60);
     pop();
+}
 
+/**
+ * Draws the cold, hard ground
+ */
+function drawGround() {
     // The ground
     push();
     noStroke();
     fill(200);
     rect(0, 400, 640, 480);
     pop();
+}
 
+/**
+ * Draws a lovely, cosy house with a pointy roof
+ */
+function drawHouse() {
+    drawBody();
+    drawRoof();
+    drawWindow();
+    drawDoor();
+}
+
+function drawBody() {
     // The main body of the house
     push();
     noStroke();
     fill(250, 250, 200);
     rect(200, 240, 280, 180);
     pop();
+}
 
-    // The roof
+/**
+ * Draws the roof of our house (a triangle)
+ */
+function drawRoof() {
     push();
     noStroke();
-    // You can also write colors in hex code in quote marks
     fill("#dc143c");
     triangle(180, 240, 340, 120, 500, 240);
     pop();
+}
 
-    // A window
+/**
+ * Draws a window on our house
+ */
+function drawWindow() {
     push();
-    // You can also write colour names from the CSS standard in quotes
-    // https://www.w3.org/wiki/CSS/Properties/color/keywords
     stroke("deeppink");
     strokeWeight(5);
     fill("blanchedalmond");
     rect(220, 260, 80, 80);
     pop();
+}
 
-    // An entrace
-
+/**
+ * Draws a door and a doorknob on our house
+ */
+function drawDoor() {
     // The door
     push();
     noStroke();
