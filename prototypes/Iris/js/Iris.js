0903ac389ea055f5636e67b12a5acc99377c7a11
sillysalmon33 <sillysalmon33@icloud.com>
2026-09-20 14:34:12 -0400

    mostly finished flower and also switched flower type

--- prototypes/Iris/js/Iris.js
+++ prototypes/Iris/js/Iris.js
@@ -29,12 +29,63 @@ function draw() {
   text(`X: ${mouseX}, Y: ${mouseY}`, 20, 30)
 
 
+  //creates clouds
+  push()
+  fill(255,255,255)
+  noStroke()
+  circle()
+  ellipse(x, y, w, [h])
+  pop()
+
+
+  //creates background petal
+   push()
+  fill(209, 93, 215)
+  noStroke()
+  bezier(230, 200, 230,330, 125, 550, 300,330)
+  pop()
+
+
+  //creates stem
   push()
   stroke(52, 173, 84)
   strokeWeight(15)
-    line(250, 500, 250, 230)
+    line(235, 500, 235, 230)
   pop()
 
+
+  //creates left petal
+  push()
+  noStroke()
+  fill(229, 113, 235)
+  translate(30, -30, )
+  bezier(250, 230, 350,270, 275, 550, 200,320)
+  pop()
+  
+  //creates right petal
+  push()
+  fill(229, 113, 235)
+  noStroke()
+  bezier(280, 200, 230,330, 125, 550, 150,175)
+  pop()
+
+  /**
+   * fixes left petal to make it look curved
+  */
+
+  //creates a circle that's the same colour as the background to hide the sharp edges
+  push()
+  noStroke() 
+  fill(0,255,255)
+  circle(150,190,150)
+  pop()
+
+  //creates a cirlce that makes the flower look curved
+  push()
+  noStroke()
+  fill(229, 113, 235) 
+  circle(210,253,130)
+  pop()
 
 
 
