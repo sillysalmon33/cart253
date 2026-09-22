ecdb69891a1e40b393061df2ea5e306bfd7f7d5f
sillysalmon33 <sillysalmon33@icloud.com>
2026-09-22 17:01:51 -0400

    started 6th project

---
+++ prototypes/prototypes2/dont_scare_him/js/dont scare him.js
@@ -0,0 +1,27 @@
+/**
+ * patients test
+ * kosta
+ * 
+ * move your mouse closer to a little guy to spook him (but dont do that, that's mean)
+
+ */
+
+"use strict";
+
+/**
+ * creates a canvas
+*/
+function setup() {
+    createCanvas(500, 500)
+}
+
+
+/**
+ * creates a background
+*/
+function draw() {
+    background(color, [a])
+
+
+    //when you move your mouse closer to a little guy (maybe the mouse has a spooky artwork attacted to it) it changes his sprite to be really scared and if you move away he does back to normal
+}
